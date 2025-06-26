9c48dc8b885d91d861ead69dc632200964e416f7
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-26 22:50:26 +0600

    pick  🚁

--- src/shared/pick.ts
+++ src/shared/pick.ts
@@ -3,7 +3,7 @@ const pick = <T extends Record<string, unknown>, k extends keyof T>(
   keys: k[]
 ): Partial<T> => {
   const finalObj: Partial<T> = {};
-
+//pick
   for (const key of keys) {
     if (obj && Object.hasOwnProperty.call(obj, key)) {
       finalObj[key] = obj[key];
